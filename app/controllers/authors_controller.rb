1cf12c795e107f699f22d6189b3ca13a2fb0c2e5
OM Miles <miles@ommiles.com>
2021-06-09 20:25:06 -0700

    lab complete

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,8 +7,23 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
+  def edit
+    @author = Author.find(params[:id])
+  end
+
   def create
     @author = Author.create(author_params)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @author = Author.find(params[:id])
+
+    @author.update(author_params)
 
     redirect_to author_path(@author)
   end
